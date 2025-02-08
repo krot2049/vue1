4b72df297d8022f34e731280f30d4de4683f3ad0
Ruslan Kozlov <ackermanqpp0912@mail.ru>
2025-02-08 08:56:17 +0700

    1

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        product: "Socks",
+        image: "../assets/vmSocks-green-onWhite.jpg",
+        link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
+        altText: "A pair of socks",
+        inStock: true,
+        inventory: 100
+    }
+ });
+ 
