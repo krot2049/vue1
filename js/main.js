f1241cd0d476ca7dfc576c82fa059e1de394427f
Ruslan Kozlov <ackermanqpp0912@mail.ru>
2025-02-08 10:11:47 +0700

    2

--- js/main.js
+++ js/main.js
@@ -6,7 +6,38 @@ let app = new Vue({
         link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
         altText: "A pair of socks",
         inStock: true,
-        inventory: 100
-    }
+        onSale: true,
+        inventory: 100,
+        cart: 0,
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green',
+                variantImage: "../assets/vmSocks-green-onWhite.jpg",
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue',
+                variantImage: "../assets/vmSocks-blue-onWhite.jpg",
+            }
+         ],
+
+
+         
+    },
+
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        
+        updateProduct(variantImage) {
+            this.image = variantImage
+         },
+     },
+    
  });
- 
+
