11f374c86a097b2395e27e28b8c0863adf5e1d14
Ruslan Kozlov <ackermanqpp0912@mail.ru>
2025-09-15 09:55:55 +0700

    zadanie3-4

--- 3/vue/js/main.js
+++ 3/vue/js/main.js
@@ -80,3 +80,20 @@ Vue.component('task-card', {
                 }
             }
         });
+
+        new Vue({
+            el: '#app',
+            data: {
+                columns: [
+                    { id: 1, title: 'Запланированные задачи' },
+                    { id: 2, title: 'Задачи в работе' },
+                    { id: 3, title: 'Тестирование' },
+                    { id: 4, title: 'Выполненные задачи' }
+                ],
+                tasks: [],
+                showCreateModal: false,
+                showEditModal: false,
+                showReasonModal: false,
+                currentTask: null,
+                taskToReturnId: null
+            },
